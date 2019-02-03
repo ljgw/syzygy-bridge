bcb9283df08542df324df60806b4ffc8f59e06bb
Laurens Winkelhagen <ljgw@users.noreply.github.com>
2019-02-03 11:46:38 +0100

    add windows DLL

--- src/test/java/com/winkelhagen/chess/syzygy/SyzygyBridgeTest.java
+++ src/test/java/com/winkelhagen/chess/syzygy/SyzygyBridgeTest.java
@@ -1,5 +1,6 @@
 package com.winkelhagen.chess.syzygy;
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 import java.io.Console;
 import java.io.File;
@@ -14,43 +15,40 @@ public class SyzygyBridgeTest {
 
     private static int loadResult;
 
-    @org.junit.BeforeClass
+    @BeforeClass
     public static void setup(){
         URL testSyzygyDirectory = SyzygyBridgeTest.class.getClassLoader().getResource("syzygy");
         String testSyzygyPath = testSyzygyDirectory.getPath().replaceAll("%20", " ");
         loadResult = SyzygyBridge.load(testSyzygyPath);
     }
 
-    @org.junit.Test
+    @Test
     public void isLibLoaded() {
         assertTrue("JSyzygy library should have been loaded", SyzygyBridge.isLibLoaded());
     }
 
-    @org.junit.Test
+    @Test
     public void isAvailable() {
         assertTrue("JSyzygy should be available for 3 pieces endgames", SyzygyBridge.isAvailable(3));
         assertFalse("JSyzygy should not be available for 4 pieces endgames", SyzygyBridge.isAvailable(4));
     }
 
-    @org.junit.Test
+    @Test
     public void load() {
         assertEquals("expected tablebase should match actual tablebase", 3, loadResult);
-
     }
 
-    @org.junit.Test
+    @Test
     public void getSupportedSize() {
         assertEquals("supported size for the tablebase is expected to be 3", 3, loadResult);
-
     }
 
-    @org.junit.Test
+    @Test
     public void probeSyzygyWDL() {
         assertEquals("KRvK should be a win in this position", SyzygyConstants.TB_WIN, SyzygyBridge.probeSyzygyWDL(3L, 4096L, 4097L, 0L, 2L, 0L, 0L, 0L, 0, true ));
-
     }
 
-    @org.junit.Test
+    @Test
     public void probeSyzygyDTZ() {
         int result = SyzygyBridge.probeSyzygyDTZ(3L, 4096L, 4097L, 0L, 2L, 0L, 0L, 0L, 0, 0, true );
         assertEquals("KRvK should win in this position", 19924244, result);
@@ -59,7 +57,6 @@ public class SyzygyBridgeTest {
         assertEquals("KRvK DTZ Result should indicate no promotion", 0, SyzygyConstants.promoteInto(result));
         assertEquals("KRvK DTZ Result should indicate 19 ply", 19, SyzygyConstants.distanceToZero(result));
         assertEquals("KRvK DTZ Result should indicate a win in this position", SyzygyConstants.TB_WIN, SyzygyConstants.winDrawLoss(result));
-
     }
 
 }
